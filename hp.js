c24a93b5557c1d643bdb4bec7db1c52913ac85d2
Aryan <aryanbpatel19@gnu.ac.in>
2024-01-23 11:20:43 +0530

    "new pushed"

---
+++ hp.js
@@ -0,0 +1,354 @@
+import React from "react";
+import Link from "next/link";
+
+const ServiceitrFaq = () => {
+  return (
+    <div>
+      <section className="faq">
+        <div className="container">
+          <div className="content">
+            <h3>
+              {" "}
+              <span> FAQ’s</span> List
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit,
+              dui quis nibh risus molestie amet. Eu vitae, blandit neque lorem
+              senectus non. Egestas eget id semper risus aenean.
+            </p>
+          </div>
+
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="acrodensfaq">
+              <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingOne">
+                    <button
+                      className="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Tortor fringilla eleifend eget tempor vestibulum id cursus
+                      ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    className="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      <ul>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingTwo">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Tortor fringilla eleifend eget tempor vestibulum id cursus
+                      ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseTwo"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      <ul>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingThree">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Tortor fringilla eleifend eget tempor vestibulum id cursus
+                      ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseThree"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingThree"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      <ul>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                        <li>
+                          {" "}
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Nisi voluptate in unde iste officia numquam
+                          obcaecati sed consequuntur architecto veniam dicta
+                          eaque sint facere nostrum vero alias excepturi,
+                          distinctio consectetur.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ServiceitrFaq;
